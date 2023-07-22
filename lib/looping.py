22788de1285368b58d652bdfb3e0ee954a51dfe7
Scott Schwab <scottschwab86@gmail.com>
2023-07-22 18:39:45 -0500

    Complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    num = 10
+    while num > 0:
+        print(num)
+        num -=1
+    print('Happy New Year!')
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list = [num * num for num in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(100):
+        num = i + 1
+        if (num % 3 == 0 and num % 5 == 0):
+            print('FizzBuzz')
+        elif(num % 3 == 0):
+            print('Fizz')
+        elif (num % 5 == 0):
+            print('Buzz')
+        else:
+            print(num)
